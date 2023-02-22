b21315a89329cf2acc4094d4444b8b563e6bf905
Luís Jesus <luisjesus__2003@outlook.com>
2023-02-22 18:03:22 +0000

    added excs lab2 part 1

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,38 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  // Retrieve the current timer configuration
+  uint8_t timer_config = 0;
+  timer_get_conf(timer, &timer_config);
+  
+  // Extract the least significant byte of the configuration
+  uint8_t timer_config_lsb = timer_config & 0x0F;
 
-  return 1;
+  // Construct the new timer configuration using the desired frequency
+  uint8_t timer_config_msb = (timer << 6) | TIMER_LSB_MSB | timer_config_lsb;
+
+  // Write the new configuration to the timer control register
+  if (sys_outb(TIMER_CTRL, timer_config_msb) != OK) {
+    // Failed to write configuration
+    return 1;
+  }
+
+  // Calculate the timer counter value needed for the desired frequency
+  uint16_t timer_counter_value = TIMER_FREQ / freq;
+
+  // Extract the least and most significant bytes of the timer counter value
+  uint8_t timer_counter_value_lsb, timer_counter_value_msb;
+  util_get_LSB(timer_counter_value, &timer_counter_value_lsb);
+  util_get_MSB(timer_counter_value, &timer_counter_value_msb);
+
+  // Write the timer counter value to the timer's counter registers
+  if (!(sys_outb(TIMER_0 + timer, timer_counter_value_lsb) == OK &&
+      sys_outb(TIMER_0 + timer, timer_counter_value_msb) == OK)) {
+
+    return 1;
+  }
+
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -32,16 +60,40 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t command = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
 
-  return 1;
+  if(sys_outb(TIMER_CTRL, command) != OK) {
+    return 1;
+  }
+
+  return util_sys_inb(TIMER_0 + timer, st);
+
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+                          
+  union timer_status_field_val val; //union to store the value of the field
 
-  return 1;
+  switch(field) {
+    case tsf_all:
+      val.byte = st;
+      break;
+    case tsf_initial:
+      st = st & (BIT(4) | BIT(5));
+      st >>= 4;
+      val.in_mode = st;
+      break;
+    case tsf_mode:
+      st = st & (BIT(1) | BIT(2) | BIT(3));
+      st >>= 1;
+      val.count_mode = st;
+      break;
+    case tsf_base:
+      st = st & BIT(0);
+      val.bcd = st;
+      break;
+  }
+  
+  return timer_print_config(timer, field, val);
 }
