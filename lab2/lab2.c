ec77e65b6f5332bf0b5ae710b82db9bf2029a885
Luís Jesus <luisjesus__2003@outlook.com>
2023-04-26 13:37:59 +0100

    finished lab5. improved lab2 and lab3.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[]) {
 
   // enables to log function invocations that are being "wrapped" by LCF
   // [comment this out if you don't want/need it]
-  lcf_trace_calls("/home/lcom/labs/lab2/trace.txt");
+  lcf_trace_calls("/home/lcom/labs/g4/lab2_rewrite/trace.txt");
 
   // enables to save the output of printf function calls on a file
   // [comment this out if you don't want/need it]
-  lcf_log_output("/home/lcom/labs/lab2/output.txt");
+  lcf_log_output("/home/lcom/labs/g4/lab2_rewrite/output.txt");
 
   // handles control over to LCF
   // [LCF handles command line arguments and invokes the right function]
@@ -30,48 +30,60 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  uint8_t st = 0;
-  if (timer_get_conf(timer, &st) != OK) return 1;
-  if (timer_display_conf(timer, st, field) != OK) return 1;
+  uint8_t st;
+  if (timer_get_conf(timer,&st) != OK)
+    return !OK;
+  
+  if (timer_display_conf(timer, st, field) != OK) 
+    return !OK;
+  
   return OK;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  if (timer_set_frequency(timer, freq) != OK) return 1;
+  if (timer_set_frequency(timer, freq) != OK)
+    return !OK;
   return OK;
 }
 
 int(timer_test_int)(uint8_t time) {
-  extern uint32_t counter;
   int ipc_status;
   int r;
-  message msg;
   uint8_t bit_no;
-  timer_subscribe_int(&bit_no);
-  uint8_t irq_set = BIT(bit_no);
-  
-  while (time > 0) { 
-        if( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
-          printf("driver_receive failed with: %d", r);
-          continue;
-        }
-        if (is_ipc_notify(ipc_status)) { /* received notification */
-            switch (_ENDPOINT_P(msg.m_source)) {
-              case HARDWARE:         
-                if (msg.m_notify.interrupts & irq_set) { 
-                  timer_int_handler();
-                  if(counter % 60 == 0){
-                    timer_print_elapsed_time();
-                    time--;
-                  }
-                }
-                break;
-              default:
-                break; /* no other notifications expected: do nothing*/
-            }
-      }
-  }
+  uint8_t irq_set;
+  message msg;
 
-  if (timer_unsubscribe_int() != 0) {return !OK;}
+  extern uint32_t counter;
+  
+  if (timer_subscribe_int(&bit_no) != OK)
+    return !OK;
+
+  irq_set = BIT(bit_no);
+
+  while(counter / 60 < time) {
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & irq_set) {
+            timer_int_handler();
+            if (counter % 60 == 0)
+              timer_print_elapsed_time();
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  } 
+
+  if (timer_unsubscribe_int() != OK) 
+    return !OK;
+  
   return OK;
+
 }
+
--- lab3/utils.c
+++ lab3/utils.c
@@ -1,27 +1,22 @@
 #include <lcom/lcf.h>
-#include <minix/sysutil.h>
+
 #include <stdint.h>
-
-int sys_inb_count = 0;
-
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   (*lsb) = (uint8_t) (val & 0x00FF);
-  return 0;
+  return OK;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   (*msb) = (uint8_t) ((val & 0xFF00) >> 8);
-
-  return 0;
+  return OK;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  uint32_t aux = 0;
-  int r = sys_inb(port, &aux);
-  #ifdef LAB3
-  sys_inb_count++;
-  #endif
-  *value = (uint8_t) aux;
-  return r;
+  uint32_t v = 0;
+  if (sys_inb(port, &v) != OK) 
+    return !OK;
+
+  (*value) = (uint8_t) v;
+  return OK;   
 }
