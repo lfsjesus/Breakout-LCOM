e241eefce66448a4c131c943bf3964845eee7767
Luís Jesus <luisjesus__2003@outlook.com>
2023-05-24 16:14:57 +0100

    detecting collisions with paddle

--- proj/src/controller/game/ball.c
+++ proj/src/controller/game/ball.c
@@ -2,7 +2,7 @@
 
 extern vbe_mode_info_t modeinfo;
 extern Brick bricks[100];
-extern Paddle* mainPaddle;
+extern Paddle mainPaddle;
 
 void collision_board(Ball* ball) {
   
@@ -36,15 +36,16 @@ void collision_board(Ball* ball) {
 }
 
 void change_ball_pos(Ball* ball) {
-  collision_board(ball);
-  collision_paddle(ball, mainPaddle);
-
-  for (int i = 0; i < 100; i++) {
+    collision_board(ball);
+    collision_paddle(ball, &mainPaddle);
+    for (int i = 0; i < 100; i++) {
     Brick* actual = &bricks[i];
     if (actual->width == 0)
-      continue;
+        continue;
     collision_brick(ball, actual);
-  }
+
+    }
+
 }
 
 void collision_brick(Ball* ball, Brick* brick) {
@@ -93,6 +94,48 @@ void collision_brick(Ball* ball, Brick* brick) {
 }
 
 void collision_paddle(Ball* ball, Paddle* paddle) {
-    // TO IMPLEMENT: collision between ball and paddle
+    // Calculate the coordinates of the ball's bounding box
+    uint16_t ball_left = ball->x - ball->radius;
+    uint16_t ball_right = ball->x + ball->radius;
+    uint16_t ball_top = ball->y - ball->radius;
+    uint16_t ball_bottom = ball->y + ball->radius;
+
+    // Calculate the coordinates of the paddle's bounding box
+    uint16_t paddle_left = paddle->x;
+    uint16_t paddle_right = paddle->x + paddle->sprite->width;
+    uint16_t paddle_top = paddle->y;
+    uint16_t paddle_bottom = paddle->y + paddle->sprite->height;
+
+    // Check for collision between the ball and paddle
+    if (ball_right >= paddle_left && ball_left <= paddle_right && ball_bottom >= paddle_top && ball_top <= paddle_bottom) {
+        // Calculate the center of the paddle
+        uint16_t paddleCenterX = paddle_left + paddle->sprite->width / 2;
+        uint16_t paddleCenterY = paddle_top + paddle->sprite->height / 2;
+
+        // Calculate the distance between the ball's center and the paddle's center
+        int16_t distanceX = ball->x - paddleCenterX;
+        int16_t distanceY = ball->y - paddleCenterY;
+
+        // Check which side of the paddle the ball collided with
+        if (abs(distanceX) >= abs(distanceY)) {
+            // Collided horizontally
+            if (distanceX > 0) {
+                ball->x = paddle_right + ball->radius;
+            } else {
+                ball->x = paddle_left - ball->radius;
+            }
+            ball->vx = -ball->vx;
+        } else {
+            // Collided vertically
+            if (distanceY > 0) {
+                ball->y = paddle_bottom + ball->radius;
+            } else {
+                ball->y = paddle_top - ball->radius;
+            }
+            ball->vy = -ball->vy;
+        }
+    }
+
+
 }
   
