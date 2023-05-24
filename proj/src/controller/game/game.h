2e0abb51663ecbe22d336d99e7d48e7e5abe215b
Luís Jesus <luisjesus__2003@outlook.com>
2023-05-24 09:20:38 +0100

    exporting project to gitlab

---
+++ proj/src/controller/game/game.h
@@ -0,0 +1,31 @@
+#ifndef GAME_CONTROLLER_H
+#define GAME_CONTROLLER_H
+
+#include <minix/sysutil.h>
+#include <lcom/lcf.h>
+#include "../../controller/keyboard_mouse/mouse.h"
+#include "../../controller/video/graphics.h"
+#include "paddle.h"
+#include "ball.h"
+#include "brick.h"
+#include "../../view/view.h"
+
+typedef enum {
+    RUNNING,
+    EXIT,
+} SystemState;
+
+typedef enum {
+    START,
+    GAME,
+    SCORE,
+    SETTINGS,
+    END
+} GameState;
+
+void update_keyboard_state();
+void update_mouse_state();
+void update_timer_state();
+void refresh_buttons_state();
+
+#endif
