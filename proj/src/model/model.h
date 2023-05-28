e8fef866aa8f19ee6b1f81fcff3d52da5d126539
antoniorama <antonio.rama.03@gmail.com>
2023-05-28 11:42:12 +0100

    model.h documentation

--- proj/src/model/model.h
+++ proj/src/model/model.h
@@ -25,43 +25,76 @@
 #include "xpm/life_power.xpm"
 #include "xpm/font/font.h"
 
+/**
+ * @brief Set up all sprite object
+ */
 void setup_sprites();
+/**
+ * @brief Destroys all the sprite objects
+ * Important to use this before ending the game loop
+ */
 void destroy_sprites();
+/**
+ * @brief Set up all the bricks for the game
+ */
 void setup_bricks();
+/**
+ * @brief Set up all the powerups for the game
+ */
 void setup_powerups();
+/**
+ * @brief Set up the ball object for the game
+ * @param n current setting
+ */
 void setup_ball(uint8_t n);
+/**
+ * @brief Set up the paddle object for the game
+ * @param n current setting
+ */
 void setup_paddle(uint8_t n);
 
+/**
+ * @brief struct used for brick objects
+ */
 typedef struct {
-    Sprite* sprite; 
-    uint16_t x;
-    uint16_t y;
-    uint8_t hp;
+    Sprite* sprite;     /**< Pointer to the sprite object representing the brick. */
+    uint16_t x;         /**< The x-coordinate of the brick. */
+    uint16_t y;         /**< The y-coordinate of the brick. */
+    uint8_t hp;         /**< The health points (HP) of the brick. */
 } Brick;
 
+/**
+ * @brief Struct used for ball objects
+ */
 typedef struct {
-    Sprite* sprite;
-    uint16_t radius;
-    uint16_t x;
-    uint16_t y;
-    int16_t vx;
-    int16_t vy;
+    Sprite* sprite;     /**< Pointer to the sprite object representing the ball. */
+    uint16_t radius;    /**< The radius of the ball. */
+    uint16_t x;         /**< The x-coordinate of the ball. */
+    uint16_t y;         /**< The y-coordinate of the ball. */
+    int16_t vx;         /**< The x-velocity of the ball. */
+    int16_t vy;         /**< The y-velocity of the ball. */
     uint8_t power;
     uint16_t speed;
 } Ball;
 
+/**
+ * @brief Struct used for paddle objects
+ */
 typedef struct {
-    Sprite* sprite;
-    uint16_t x;
-    uint16_t y;
+    Sprite* sprite;     /**< Pointer to the sprite object representing the paddle. */
+    uint16_t x;         /**< The x-coordinate of the paddle. */
+    uint16_t y;         /**< The y-coordinate of the paddle. */
 } Paddle;
 
+/**
+ * @brief Struct used for powerup objects
+ */
 typedef struct {
-    Sprite* sprite;
-    uint16_t x;
-    uint16_t y;
-    uint8_t type;
-    bool active;
+    Sprite* sprite;     /**< Pointer to the sprite object representing the powerup. */
+    uint16_t x;         /**< The x-coordinate of the powerup. */
+    uint16_t y;         /**< The y-coordinate of the powerup. */
+    uint8_t type;       /**< The type of the powerup. */
+    bool active;        /**< If the powerup is active or not. */
 } PowerUp;
 
 typedef enum {
@@ -69,12 +102,21 @@ typedef enum {
     KEYBOARD,
 } ControlDevice;
 
+/**
+ * @brief Variable that holds the current number of bricks in the game
+ */
 static int brick_counter = 0;
 
+/**
+ * @brief Gets the current number of bricks in the game
+ * 
+ * @return int - current number of bricks in the game
+ */
 int getBrickCounter();
 
+/**
+ * @brief decrements the brick_counter variable by 1
+ */
 void decrementBrickCounter();
-
-
 
 #endif
