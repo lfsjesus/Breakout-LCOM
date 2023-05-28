95ed3e31275598be0d5828d5d079cf68b798e797
antoniorama <antonio.rama.03@gmail.com>
2023-05-28 12:13:04 +0100

    view documentation

--- proj/src/view/view.h
+++ proj/src/view/view.h
@@ -11,30 +11,63 @@
 
 #define TRANSPARENT 0xFFFFFE
 
+/**
+ * @brief Draws the sprite for the mouse
+ */
 void draw_mouse();
-
+/**
+ * @brief Draws the sprite for the ball
+ */
 void draw_ball();
-
+/**
+ * @brief Draws the sprites for all the bricks 
+ * according to brick's HP (health-points)
+ */
 void draw_bricks();
-
+/**
+ * @brief Draws the sprite for the points (all digits)
+ */
 void draw_points();
-
+/**
+ * @brief Draws the sprite for the lives of the player
+ */
 void draw_lives();
-
+/**
+ * @brief Draws the sprite for all the powerups that are active
+ */
 void draw_active_powerups();
-
+/**
+ * @brief Draws the sprite for the settings screen
+ */
 void draw_setting_screen();
-
+/**
+ * @brief Draws the sprite for the game instructions
+ */
 void draw_instruction();
-
+/**
+ * @brief Draws the sprite for text
+ * @param text text to draw
+ * @param x x-coordinate to draw
+ * @param y y-coordinate to draw
+ */
 void draw_text(char *text, uint16_t x, uint16_t y);
-
+// Not being used
 int draw_scaled_sprite_xpm(Sprite *sprite, int x, int y);
-
+/**
+ * @brief Draws sprite xpm
+ * @param sprite pointer to the sprite to draw
+ * @param x x-coordinate to draw
+ * @param y y-coordinate to draw
+ * @return int (0 on success and 1 on failure)
+ */
 int draw_sprite_xpm(Sprite *sprite, int x, int y);
-
+/**
+ * @brief Draws the frame according to the game state
+ */
 void draw_new_frame();
-
+/**
+ * @brief Clears the screen
+ */
 void clear_screen();
 
 #endif
