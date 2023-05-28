d7b81ef0ec8745a5f536ddcce0dd5e9f370787d8
antoniorama <antonio.rama.03@gmail.com>
2023-05-28 14:05:04 +0100

    game controller documentation
    keyboard_move_paddle missing on header file

--- proj/src/controller/game/ball.h
+++ proj/src/controller/game/ball.h
@@ -9,22 +9,56 @@
 #include "paddle.h"
 #include "powerup.h"
 
+/**
+ * @brief Changes the position of the ball
+ * @param ball pointer to the ball
+ */
 void change_ball_pos(Ball* ball);
-
+/**
+ * @brief Handles ball collisions with edges of the screen
+ * @param ball pointer to the ball
+ */
 void collision_board(Ball* ball);
-
+/**
+ * @brief Handles ball-brick collisions
+ * @param ball pointer to the ball
+ * @param brick pointer to the brick
+ */
 void collision_brick(Ball* ball, Brick* brick);
-
+/**
+ * @brief Handles ball-extraBall collisions
+ * @param ball pointer to the ball
+ * @param extraBall pointer to the extra ball
+ */
 void collision_extra_ball(Ball* ball, Ball* extraBall);
-
+/**
+ * @brief Handles ball-paddle collisions
+ * @param ball pointer to the ball
+ * @param paddle pointer to the paddle
+ */
 void collision_paddle(Ball* ball, Paddle* paddle);
-
-bool check_ball_out(Ball* bal, Paddle* paddle);
-
+/**
+ * @brief Checks if ball is out of the screen
+ * @param ball pointer to the ball
+ * @return true if ball is out of the screen
+ * @return false if ball is inside the screen
+ */
+bool check_ball_out(Ball* ball, Paddle* paddle);
+/**
+ * @brief Resets the ball
+ * @param ball pointer to the ball
+ */
 void reset_ball(Ball* ball);
-
+/**
+ * @brief Adds a new ball to the game
+ * @param mainBall pointer to the main ball
+ * @param newBall pointer to the new ball
+ */
 void add_ball(Ball* mainBall, Ball* newBall);
-
+/**
+ * @brief Disables a ball 
+ * @param ball pointer to the ball
+ */
 void disable_extra_ball(Ball* ball);
 
 #endif
--- proj/src/controller/game/powerup.h
+++ proj/src/controller/game/powerup.h
@@ -5,16 +5,34 @@
 #include "paddle.h"
 #include "ball.h"
 
+/**
+ * @brief Drops a random powerup from a random position
+ */
 void drop_random_powerup();
-
+/**
+ * @brief Sets the powerup active to false 
+ * @param powerup pointer to the powerup
+ */
 void disable_powerup(PowerUp *powerup);
-
+/**
+ * @brief Activates the power up according to its type
+ * @param powerup pointer to the powerup
+ */
 void activate_powerup(PowerUp *powerup);
-
+/**
+ * @brief Moves all active powerup down the screen
+ */
 void move_active_powerups();
-
+/**
+ * @brief Handles active power ups collision with the paddle
+ * @param paddle pointer to the paddle
+ */
 void check_powerup_collision(Paddle *paddle);
-
+/**
+ * @brief Checks if extra ball is active
+ * @return true if extra ball is active
+ * @return false if extra ball is not active
+ */
 bool extra_ball_active();
 
 #endif
