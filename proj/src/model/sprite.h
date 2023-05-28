347410ecfe388c8a21dfcea95a0d4d569644db59
antoniorama <antonio.rama.03@gmail.com>
2023-05-28 11:56:28 +0100

    sprite.h documentation

--- proj/src/model/sprite.h
+++ proj/src/model/sprite.h
@@ -3,16 +3,27 @@
 
 #include "../controller/video/graphics.h"
 
+/**
+ * @brief Struct for Sprite objects
+ */
 typedef struct {
-    uint16_t height;
-    uint16_t width;
-    uint32_t *colors;
+    uint16_t height;        /**< Height of the sprite. */
+    uint16_t width;         /**< Width of the sprite. */
+    uint32_t *colors;       /**< Pointer to the color map of the sprite. */
     uint32_t color;
     uint8_t pressed;
 } Sprite; 
 
+/**
+ * @brief Builds a sprite with a XPM with colors in direct mode
+ * @param sprite 
+ * @return pointer to the sprite created
+ */
 Sprite *create_sprite_xpm(xpm_map_t sprite);
-Sprite *create_sprite_button(uint16_t width, uint16_t height, uint32_t color);
+/**
+ * @brief Destroys a sprite
+ * @param sprite a pointer to the sprite that will be destroyed
+ */
 void destroy_sprite(Sprite *sprite);
 
 #endif
