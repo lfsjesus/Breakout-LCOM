4d4ea9fb2845d5710ade98747f9cdf6f5e946ee6
Luís Jesus <luisjesus__2003@outlook.com>
2023-05-28 17:31:01 +0100

    fix

--- proj/src/model/sprite.h
+++ proj/src/model/sprite.h
@@ -12,7 +12,6 @@ typedef struct {
 } Sprite; 
 
 Sprite *create_sprite_xpm(xpm_map_t sprite);
-Sprite *create_sprite_button(uint16_t width, uint16_t height, uint32_t color);
 void destroy_sprite(Sprite *sprite);
 
 #endif
